733281de81ae83165a4299c056fce967bb1c11b3
missionctrlbiz <teewhy_007@yahoo.com>
2024-05-10 21:00:17 +0100

    Change BE 8000

--- frontend/src/header/NavBar.jsx
+++ frontend/src/header/NavBar.jsx
@@ -1,4 +1,4 @@
-import Container from 'react-bootstrap/Container';
+import { Container } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
