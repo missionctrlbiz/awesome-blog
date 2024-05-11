b102b7d95e70f6b84559c7b52ba47e6745e6150f
missionctrlbiz <teewhy_007@yahoo.com>
2024-05-11 13:58:24 +0100

    fixed return

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,35 +4,31 @@ import CarouselSlide from '../components/carousel/CarouselSlide'
 import NavBar from '../header/NavBar'
 import BlogList from '../components/bloglist/BlogList'
 
+const HomePage = () => {
+  const [movies, setMovies] = useState([])
 
-// const HomePage = () => {
-//     const [movies, setMovies] = useState([])
-//     useEffect(() => {
-//         fetch('http://localhost:8000/api/posts')
-//        .then(res => res.json())
-//        .then(data => setMovies(data))
-//     }, [])
-//fetch all movies
-    const fetchMovies = async () => {
-        try {
-            const res = await fetch('http://localhost:7000/api/posts')
-            const data = await res.json()
-            setMovies(data)
-        } catch (error) {
-            console.error(error)
-        }
+  const fetchMovies = async () => {
+    try {
+      const res = await fetch('http://localhost:7000/api/posts')
+      const data = await res.json()
+      setMovies(data)
+    } catch (error) {
+      console.error(error)
     }
-    useEffect(() => {
-        fetchMovies()
-    }, [])
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+
   return (
     <>
-    <NavBar />
-    <CarouselSlide />
-    {/* <div className='container'>
-    <BoxOffice />
-    </div>     */}
-    <BlogList movies={movies} />
+      <NavBar />
+      <CarouselSlide />
+      {/* <div className='container'>
+        <BoxOffice />
+      </div> */}
+      <BlogList movies={movies} />
     </>
   )
 }
